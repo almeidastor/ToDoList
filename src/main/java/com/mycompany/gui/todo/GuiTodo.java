88d75cd15b86ef74717149a9ac6836b37b8d8b11
AlmeidaStor <regianealmeidastor@gmail.com>
2023-01-29 22:21:36 -0300

    Create GuiTodo.java

---
+++ src/main/java/com/mycompany/gui/todo/GuiTodo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.gui.todo;
+
+/**
+ *
+ * @author Stor
+ */
+public class GuiTodo {
+
+    public static void main(String[] args) {
+    telaPrincipal telaPrincipal = new telaPrincipal();
+     telaPrincipal.setVisible (true);
+    }
+}
